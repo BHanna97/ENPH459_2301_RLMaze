7dfff8fed48157252756fbf24d7c5df8121e9057
Thomas Deckers <thomasdeck14@gmail.com>
2022-04-03 20:40:46 -0700

    added masking on platform straightening. Reasonable implementation of ball tracking based on proximity of thresholded light and dark parts of the image

--- scripts/process_frames.py
+++ scripts/process_frames.py
@@ -20,7 +20,6 @@ def callback(data):
     #rospy.loginfo(dt.microsecond/1000)
     #cv2.imshow('cv_img',cv_image)
     #print("Image recieved")
-    cv2.imwrite('test_hsv.jpg',cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV))
     cv2.imwrite('test.jpg', cv_image)
     get_command(cv_image)
 
@@ -29,19 +28,8 @@ def callback(data):
 #Otherwise, publish the desired angle of the plaform to target_platform_angle
 def get_command(raw_image):
     platform_image=crop_platform(raw_image)	
-   # ball_found, ball_loc=track_ball(platform_image)
+    ball_found, ball_loc=track_ball(platform_image)
     return False
-
-#Takes a raw image of the scene and returns a cropped image of only a rectangular platform
-def crop_platform(raw_image):
-    #edges=cv2.Canny(raw_image, 100, 200, apertureSize=3, L2gradient = True) #doesn't seem very effective
-    #cv2.imshow('lines',edges)
-    #cv2.waitKey(1)
-    identify_tape(raw_image)
-    #M = cv2.getPerspectiveTransform(np.float32([[0,442],[250,693],[650,271],[453,22]]),np.float32([[0,250],[0,50],[300,50],[300,250]]))
-    #straight = cv2.warpPerspective(raw_image, M, (300,300), flags = cv2.INTER_LINEAR)
-    #cv2.imshow('straightened', straight)
-    return(raw_image)
 
 #Takes in cropped and straightened image of platform and returns ball state
 #dev note: after implementing quite a bit of framework to use a tracking algorithm
@@ -58,9 +46,11 @@ def track_ball(platform_image):
             ball_on_platform=True
             rospy.loginfo('ball found!')
     else: #we know where the ball is, we don't need to search the whole platform
-        (x,y,_,_)= [int(v) for v in ball_loc]
-        local_image = platform_image #h[y-40:y+40,x-40:x+40]
-        ball_found, box = identify_ball(local_image)
+        search_rad=100
+        (x,y,w,h)= [int(v) for v in ball_loc]
+        local_image = platform_image[y-search_rad:y+search_rad,x-search_rad:x+search_rad]
+        ball_found, ball_loc = identify_ball(local_image)
+        ball_loc = [ball_loc[0]+x-search_rad,ball_loc[1]+y-search_rad,ball_loc[2],ball_loc[3]]
         if not ball_found:
             #TODO: add some inertia here so that one failed frame doesn't trigger a reset
             ball_on_platform=False
@@ -70,7 +60,6 @@ def track_ball(platform_image):
     #debug
     tracked=cv2.rectangle(platform_image, (x,y), (x+w, y+h), (0,255,0), 2)
     cv2.imshow('box',tracked)
-    cv2.imshow('raw',platform_image)
     cv2.waitKey(1)
 
     if ball_found:
@@ -79,52 +68,69 @@ def track_ball(platform_image):
         return False, None
 
 def identify_ball(image):
-    img_bw=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     #may want to gaussian blur here
     #TODO:tune
-    thresh_high = cv2.thresh(img_bw, 50,255, cv2.THRESH_BINARY)
-    thresh_low = cv2.thresh(img_bw, 200,255, cv2.THRESH_BINARY_INV)
-    erosion_kernel = np.ones((5,5), np.uint8)
-    thresh_eroded = cv2.erode(thresh,erosion_kernel, iterations = 3) #TODO: tune
-    cv2.imshow('threshold', thresh)
-    cv2.imshow('hsv',img_hsv)
-    cv2.imshow('thresh_eroded', thresh_eroded)
+    _,thresh_high = cv2.threshold(image, 215,255, cv2.THRESH_BINARY)
+    _,thresh_low = cv2.threshold(image, 125,255, cv2.THRESH_BINARY_INV)
+
+    kernel = np.ones((5,5), np.uint8)
+    try:
+        high_dilated = cv2.dilate(thresh_high,kernel, iterations = 1) #TODO: tune
+        low_dilated = cv2.dilate(thresh_low,kernel, iterations = 1) #TODO: tune
+        ball_mask=cv2.bitwise_and(high_dilated,low_dilated)
+
+        cv2.imshow('brightest', high_dilated)
+        cv2.imshow('undilated', thresh_high)
+        cv2.imshow('darkest', thresh_low)
+    except:
+        rospy.loginfo('thresh blanked')
+        ball_mask=0
+
+
+    cv2.imshow('ball_mask', ball_mask)
     cv2.waitKey(1)
 
     #print(np.sum(thresh_eroded))
-    if (np.sum(thresh_eroded) > 500): #TODO:Tune
+    if (np.sum(ball_mask) > 2000000): #TODO:Tune
     #    contours=cv2.findContours(thresh_eroded,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-        return(True, cv2.boundingRect(thresh_eroded))
+        return(True, cv2.boundingRect(ball_mask))
     else:
         return False, [-1,-1,-1,-1]
 
-def identify_tape(image):
-    #Bounding regions of tape, as x,y,w,h, found experimentally
+def crop_platform(image):
+    global maze_mask
+    global prev_marker_centroids
+    #Bounding regions of tape, as x,y,w,h, starting at the top left and going clockwise
+    #found experimentally
     #TODO: tune to smaller bounding boxes
-    topl=[0,350,100,150]
-    topr=[350,0,150,200]
-    botl=[200,550,150,150]
-    botr=[550,200,200,150]
-    topl_c=get_tape_centroid(image, topl)
-    topr_c=get_tape_centroid(image, topr)
-    botl_c=get_tape_centroid(image, botl)
-    botr_c=get_tape_centroid(image, botr)
+
+    marker_boxes=[
+    [350,0,150,200],
+    [550,200,200,150],
+    [0,350,100,150],
+    [200,550,150,150],
+    ]
+    marker_centroids=[[0,0],[0,0],[0,0],[0,0]]
+    for i in range(0,4):
+        marker_centroids[i]=get_tape_centroid(image, marker_boxes[i])
+        if marker_centroids[i]==None:
+            marker_centroids[i]=prev_marker_centroids[i]
+    prev_marker_centroids=marker_centroids 
+
+    #test_cols=((255,0,0),(0,255,0),(0,0,255),(255,255,255))
+    #for i in range(0,4):
+    #    cv2.circle(image, marker_centroids[i],2, test_cols[i], -1) 
+    #    cv2.rectangle(image, marker_boxes[i],test_cols[i] ,1)
+    #    cv2.imshow('image', image)
+    #    cv2.waitKey(1)
     
-    dots = cv2.circle(image, topl_c,3, (0,0,255), -1) 
-    dots = cv2.circle(dots, topr_c,3, (0,255,255), -1) 
-    dots = cv2.circle(dots, botl_c,3, (255,0,255), -1) 
-    dots = cv2.circle(dots, botr_c,3, (255,255,255), -1) 
-    bbs=cv2.rectangle(dots,topl, (0,0,255),1)
-    bbs=cv2.rectangle(dots,topr, (0,255,255),1)
-    bbs=cv2.rectangle(dots,botl, (255,0,255),1)
-    bbs=cv2.rectangle(dots,botr, (255,255,255),1)
-
-    cv2.imshow('dots', dots)
-    cv2.waitKey(1)
-
-    ptransform = cv2.getPerspectiveTransform(np.float32([topl_c,botl_c,botr_c,topr_c]),np.float32([[37,319],[215,535],[660,300],[474,24]]))
-    straight = cv2.warpPerspective(image, ptransform, (678,577), flags = cv2.INTER_LINEAR)
-    cv2.imshow('straightened', straight)
+    image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ptransform = cv2.getPerspectiveTransform(np.float32(marker_centroids),np.float32([[474,24],[660,300],[37,319],[215,535]]))
+    straight = cv2.warpPerspective(image_bw, ptransform, (678,577), flags = cv2.INTER_LINEAR)
+    straight_clean = cv2.bitwise_and(straight,straight,mask=maze_mask)
+    #cv2.imshow('straightened', straight_clean)
+    
+    return straight_clean
 
 def crop(img, bb):
     return  img[bb[1]:bb[1]+bb[3],bb[0]:bb[0]+bb[2]]
@@ -137,9 +143,11 @@ def get_tape_centroid(image,bb):
     #cv2.imshow('local_image',local_image)
     #cv2.imshow('thresh', thresh)
     cv2.waitKey(1)
-    cent_local =  [int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])]
-    
-    return [bb[0]+cent_local[0],bb[1]+cent_local[1]]
+    if M["m00"]!=0: 
+        cent_local =  [int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])]
+        return [bb[0]+cent_local[0],bb[1]+cent_local[1]]
+    else:
+        return None
 
 def listener():
     
@@ -157,4 +165,7 @@ if __name__ == '__main__':
     #tracker = cv2.TrackerMIL_create()
     global ball_on_platform
     ball_on_platform = False
+    global maze_mask
+    maze_mask=cv2.imread('/home/pi/ros_catkin_ws/src/project_theseus/scripts/straightened_platform_mask.jpg',cv2.IMREAD_GRAYSCALE)
+
     listener()
